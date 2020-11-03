3c8d4bd4155bb06694d8602e9b1e069123f9651b
ElenaBubblez <elenastavrou8@hotmail.com>
2020-11-03 17:50:25 +0000

    does not work but trying to display cards

--- Task-Planner-JS/Task-Planner.js
+++ Task-Planner-JS/Task-Planner.js
@@ -1,3 +1,7 @@
+
+let button = document.getElementById('#buttonClick');
+
+
 //validate form input.
 function validateTaskForm(){
     let name = document.querySelector('#assignedBy').value;
@@ -6,92 +10,124 @@ function validateTaskForm(){
     let date = document.querySelector('#dueDate').value;
     let status = document.querySelector('#inputStatus').value;
     
+    let score = 0;
+    
     let isValid = false;
     
     if(name.length > 5 && name.length != -1){
-        isValid = true;
+        //isValid = true;
+        score ++;
     }
     else{
-        isValid = false;
         document.getElementById("error1").innerHTML = "Text needs to be more than 5 characters";
     }
     if(assignedTo.length > 5 && assignedTo.length != -1){
-        isValid = true;
+        //isValid = true;
+        score++;
     }
     else{
-        isValid = false;
         document.getElementById("error2").innerHTML = "Text needs to be more than 10 characters";
     }
     if(description.length > 10 && description.length != -1){
-        isValid = true;
+        //isValid = true;
+        score++;
     }
     else{
-        isValid = false;
         document.getElementById("error3").innerHTML = "Text needs to be more than 5 characters";
     }
     if(date.length != -1){
-        isValid = true;
+        //isValid = true;
+        score++;
     }
     else {
-        isValid = false;
         document.getElementById("error4").innerHTML = "Text needs to be more than 8 characters";
     }
 
     if(status.length != -1){
-        isValid = true;
+        //isValid = true;
+        score++;
     }
     else {
-        isValid = false;
         document.getElementById("error5").innerHTML = "Text needs to be more than 8 characters";
     }
 
+    if(score == 5){
+        isValid = true;
+    }
+    console.log(isValid);
+        return isValid;
 }
 
+validateTaskForm();
 
-let taskObj = {
-    id: undefined,
-    name: "",
-    description: "",
-    assignedTo: "",
-    date: undefined,
-    status: "" 
-}
 
-class TaskManager{
+let taskArray = [];
+let id;
+
+
+
+
+    class TaskManager{
+
+        createTaskObj(name, description, assignedTo, date, status){
+            let taskObj ={
+            "id": `${taskManager.taskArray.length < 1 ? 1: taskManager.taskArray.length+1}`,
+            "name": name,
+            "description": description,
+            "assignedTo": assignedTo,
+            "date": date,
+            "status": status
+            }
+            taskArray.push(taskObj);
+        }
+
     //returns the list of all task
     getAllTask(){
-        
+
     }
 
     //returns a list of all task that status is equal to the status passes as an argument:
     getTasksWithStatus(){
-
+        
     }
 
     //add task to existing task list
-    addTask(){
-        `<div class="row justify-content-center">
-            <div class="col-md-2 d-flex justify-content-center">
-                <div class="card" style="width: 18rem;">
-                    <div class="card-header">
-                    Task
-                    </div>
-                    let taskHTML = <div class="card-body">
-                    <h6 class="card-title">Assigned To:</h5>
-                    <p class="card-text" id="showAssignedTo">${assignedTo}</p>
-                    <h6 class="card-title">Assigned By:</h5>
-                    <p class="card-text" id="showAssignedBy">${name}</p>
-                    <h6 class="card-title">Due Date:</h5>
-                    <p class="card-text" id="showDueDate">${date}</p>
-                    <h6 class="card-title">Status:</h5>
-                    <p class="card-text" id="showStatus">${status}</p>
-                    <h6 class="card-title">Description:</h5>
-                    <p class="card-text" id="showDescription">${description}</p>
-                </div>
-            </div>
-        </div>
-    </div> `
+    addTask(item){
+
+       
+
+        //interpolates the user input into the shown card
+        const cardContent  =  `<div class="card" id="${item.id} style="width: 18rem;">
+                            <div class="card-header">
+                            Task
+                            </div>
+                            <div class="card-body" >
+                                <h6 class="card-title">Assigned To:</h5>
+                                <p class="card-text" id="showAssignedTo">${item.assignedTo}</p>
+                                <h6 class="card-title">Assigned By:</h5>
+                                <p class="card-text" id="showAssignedBy">${item.name}</p>
+                                <h6 class="card-title">Due Date:</h5>
+                                <p class="card-text" id="showDueDate">${item.date}</p>
+                                <h6 class="card-title">Status:</h5>
+                                <p class="card-text" id="showStatus">${item.status}</p>
+                                <h6 class="card-title">Description:</h5>
+                                <p class="card-text" id="showDescription">${item.description}</p>
+                            </div>
+                        </div> `;
+
+            //const position = "beforeend";
+            let listItem = document.getElementById('list-items').value;
+             // Append newyly created card element to the container
+            listItem.insertAdjacentHtML('beforeend', cardContent); // inserts item at bottom of the page
+
+        //});
+
+        
     }
+
+
+    
+    
 
     //deletes task from task list
     deleteTask(){
@@ -103,11 +139,17 @@ class TaskManager{
 
     }
     
-
-
-
-
+    
 
 
 }
+button.addEventListener("click", function(){
 
+    if(validateTaskForm().isValid) { 
+
+    createTaskObj(name, description, assignedTo, date, status);
+    addTask(item);
+    
+    }
+
+})
