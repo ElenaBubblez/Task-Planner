d8c54dd4b57234d2ddf41e9c1ae0fe28d7baad54
ElenaBubblez <elenastavrou8@hotmail.com>
2020-11-04 16:21:35 +0000

    adding cards works!

--- Task-Planner-JS/Task-Planner.js
+++ Task-Planner-JS/Task-Planner.js
@@ -1,5 +1,5 @@
 
-let button = document.getElementById('#buttonClick');
+let deleteButton = document.getElementById('#deleteButton');
 
 
 //validate form input.
@@ -14,76 +14,85 @@ function validateTaskForm(){
     
     let isValid = false;
     
-    if(name.length > 5 && name.length != -1){
-        //isValid = true;
+    if(name.length > 3 && name.length != -1){
         score ++;
     }
     else{
         document.getElementById("error1").innerHTML = "Text needs to be more than 5 characters";
     }
-    if(assignedTo.length > 5 && assignedTo.length != -1){
-        //isValid = true;
+    if(assignedTo.length > 3 && assignedTo.length != -1){
         score++;
     }
     else{
         document.getElementById("error2").innerHTML = "Text needs to be more than 10 characters";
     }
-    if(description.length > 10 && description.length != -1){
-        //isValid = true;
+    if(description.length > 5 && description.length != -1){
         score++;
     }
     else{
         document.getElementById("error3").innerHTML = "Text needs to be more than 5 characters";
     }
-    if(date.length != -1){
-        //isValid = true;
+    if(date){
         score++;
     }
     else {
-        document.getElementById("error4").innerHTML = "Text needs to be more than 8 characters";
+        document.getElementById("error4").innerHTML = "Please input a date";
     }
 
-    if(status.length != -1){
-        //isValid = true;
+    if(status != "Choose"){
         score++;
     }
     else {
-        document.getElementById("error5").innerHTML = "Text needs to be more than 8 characters";
+        document.getElementById("error5").innerHTML = "Please choose status of task";
     }
 
     if(score == 5){
         isValid = true;
     }
     console.log(isValid);
-        return isValid;
+
+    if(isValid == true)
+    {
+        createTaskObj(name, description, assignedTo, date, status, taskArray);
+        let taskIndex = myTaskManager.allTasks.length-1;
+        myTaskManager.addTask(myTaskManager.allTasks[taskIndex]);
+
+    }
 }
 
-validateTaskForm();
 
 
-let taskArray = [];
-let id;
+//function to create task object of user input
+function createTaskObj(name, description, assignedTo, date, status, taskArray){
+
+    myTaskManager.allTasks.push({
+    //checks if id has been created, if not then create id 1, if id is already created then next id is current length +1
+    "id": `${taskArray.length < 1 ? 1: taskArray.length+1}`,
+    "name": name,
+    "description": description,
+    "assignedTo": assignedTo,
+    "date": date,
+    "status": status
+    })
+    console.log(taskArray);
+    return myTaskManager.allTasks;
+}
 
 
 
 
     class TaskManager{
 
-        createTaskObj(name, description, assignedTo, date, status){
-            let taskObj ={
-            "id": `${taskManager.taskArray.length < 1 ? 1: taskManager.taskArray.length+1}`,
-            "name": name,
-            "description": description,
-            "assignedTo": assignedTo,
-            "date": date,
-            "status": status
-            }
-            taskArray.push(taskObj);
-        }
+        constructor(array, names)
+        {
+            this.allTasks = array;
+            this.names = names;
+   
 
+        }
     //returns the list of all task
     getAllTask(){
-
+        console.log(this.allTasks);
     }
 
     //returns a list of all task that status is equal to the status passes as an argument:
@@ -92,37 +101,51 @@ let id;
     }
 
     //add task to existing task list
-    addTask(item){
+    addTask(taskObj){
 
        
-
         //interpolates the user input into the shown card
-        const cardContent  =  `<div class="card" id="${item.id} style="width: 18rem;">
-                            <div class="card-header">
-                            Task
-                            </div>
-                            <div class="card-body" >
-                                <h6 class="card-title">Assigned To:</h5>
-                                <p class="card-text" id="showAssignedTo">${item.assignedTo}</p>
-                                <h6 class="card-title">Assigned By:</h5>
-                                <p class="card-text" id="showAssignedBy">${item.name}</p>
-                                <h6 class="card-title">Due Date:</h5>
-                                <p class="card-text" id="showDueDate">${item.date}</p>
-                                <h6 class="card-title">Status:</h5>
-                                <p class="card-text" id="showStatus">${item.status}</p>
-                                <h6 class="card-title">Description:</h5>
-                                <p class="card-text" id="showDescription">${item.description}</p>
-                            </div>
-                        </div> `;
+        const cardContent  =  `<div class="col-md-2 d-flex justify-content-center" id=${taskObj.id}>
+                                    <div class="card" style="width: 18rem;">
+                                        <div class="card-header">
+                                        Task
+                                        </div>
+                                        <div class="card-body" >
+                                            <h6 class="card-title">Assigned To:</h5>
+                                            <p class="card-text" id="showAssignedTo">${taskObj.assignedTo}</p>
+                                            <h6 class="card-title">Assigned By:</h5>
+                                            <p class="card-text" id="showAssignedBy">${taskObj.name}</p>
+                                            <h6 class="card-title">Due Date:</h5>
+                                            <p class="card-text" id="showDueDate">${taskObj.date}</p>
+                                            <h6 class="card-title">Status:</h5>
+                                            <p class="card-text" id="showStatus">${taskObj.status}</p>
+                                            <h6 class="card-title">Description:</h5>
+                                            <p class="card-text" id="showDescription">${taskObj.description}</p>
+                                        </div>
+                                        <div>
+                                            <button  type="button" onclick="deleteTask()" id="deleteButton" class="btn btn-primary">Delete</button>
+                                        </div>
+                                    </div> 
+                                </div>`;
 
-            //const position = "beforeend";
-            let listItem = document.getElementById('list-items').value;
-             // Append newyly created card element to the container
-            listItem.insertAdjacentHtML('beforeend', cardContent); // inserts item at bottom of the page
+            let cardRow = document.getElementById('rowID');
+            console.log(cardRow);
+             // Append newyly created card element to the cardContent
+            cardRow.insertAdjacentHTML('beforeEnd', cardContent); // inserts item at bottom of the page
+            
 
-        //});
+            //interpolates the user input into the shown list
+            const listContent = `<div class="list-group" role="tablist">
+                                    <a class="list-group-item list-group-item-action" id=${taskObj.id} data-toggle="list" href="#list-home" role="tab">
+                                    <h6>Assigned To: ${taskObj.assignedTo}</h6>
+                                    <p>Due Date: ${taskObj.date}</p>
+                                    <p>Status: ${taskObj.status}</p>
+                                    </a>
+                                </div>`;
 
-        
+            let listCol = document.getElementById('listID');
+            console.log(listCol);
+            listCol.insertAdjacentHTML('afterBegin', listContent);
     }
 
 
@@ -130,7 +153,17 @@ let id;
     
 
     //deletes task from task list
-    deleteTask(){
+    deleteTask(element){
+        // let thisTaskID = element.parentNode.parentNode.attributes.id.value;
+
+        // for(i = 0; i < this.allTasks.Length; i++){
+        //     if(this.allTasks[i].ID = thisTaskID){
+        //         this.allTasks.splice(i, 1);
+        //     }
+        // }
+
+        // element.parentNode.parentNode.parentNode.removeChild(element.parentNode.parentNode);
+
 
     }
 
@@ -143,13 +176,10 @@ let id;
 
 
 }
-button.addEventListener("click", function(){
 
-    if(validateTaskForm().isValid) { 
 
-    createTaskObj(name, description, assignedTo, date, status);
-    addTask(item);
-    
-    }
+//array to store input
+let taskArray = [];
 
-})
+let myTaskManager = new TaskManager(taskArray, "todoTask");
+
